72bf26c618030a43bf2a78fbd84ea9d5d5aba0ad
Vencislav <vencislav67@hotmail.com>
2022-06-21 11:51:03 +0300

    5

--- CopyDirectory/CopyDirectory.cs
+++ CopyDirectory/CopyDirectory.cs
@@ -1,6 +1,8 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
+
     public class CopyDirectory
     {
         static void Main()
@@ -13,7 +15,15 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            throw new NotImplementedException();
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach(string file in files)
+            {
+
+            }
+
         }
     }
 }
